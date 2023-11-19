2cf338f00aad12ed3d5e6c7390ba9a6da78a3462
reocat <ng.ct_ml@tuta.io>
2023-11-19 22:35:09 +0300

    vmscan

--- mm/vmscan.c
+++ mm/vmscan.c
@@ -3423,26 +3423,7 @@ static void kswapd_try_to_sleep(pg_data_t *pgdat, int order,
 	prepare_to_wait(&pgdat->kswapd_wait, &wait, TASK_INTERRUPTIBLE);
 
 	/* Try to sleep for a short interval */
-<<<<<<< HEAD
-	if (prepare_kswapd_sleep(pgdat, order, remaining,
-						balanced_classzone_idx)) {
-		/*
-		 * Compaction records what page blocks it recently failed to
-		 * isolate pages from and skips them in the future scanning.
-		 * When kswapd is going to sleep, it is reasonable to assume
-		 * that pages and compaction may succeed so reset the cache.
-		 */
-		reset_isolation_suitable(pgdat);
-
-		/*
-		 * We have freed the memory, now we should compact it to make
-		 * allocation of the requested order possible.
-		 */
-		wakeup_kcompactd(pgdat, order, classzone_idx);
-
-=======
 	if (prepare_kswapd_sleep(pgdat, order, remaining, classzone_idx)) {
->>>>>>> 77c531f66919 (simple_lmk: Make reclaim deterministic)
 		remaining = schedule_timeout(HZ/10);
 		finish_wait(&pgdat->kswapd_wait, &wait);
 		prepare_to_wait(&pgdat->kswapd_wait, &wait, TASK_INTERRUPTIBLE);
@@ -3452,12 +3433,7 @@ static void kswapd_try_to_sleep(pg_data_t *pgdat, int order,
 	 * After a short sleep, check if it was a premature sleep. If not, then
 	 * go fully to sleep until explicitly woken up.
 	 */
-<<<<<<< HEAD
-	if (prepare_kswapd_sleep(pgdat, order, remaining,
-						balanced_classzone_idx)) {
-=======
 	if (prepare_kswapd_sleep(pgdat, order, remaining, classzone_idx)) {
->>>>>>> 77c531f66919 (simple_lmk: Make reclaim deterministic)
 		trace_mm_vmscan_kswapd_sleep(pgdat->node_id);
 
 		/*
